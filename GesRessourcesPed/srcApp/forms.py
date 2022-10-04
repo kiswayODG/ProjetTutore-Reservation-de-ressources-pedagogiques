80b72f82f9bbb721cb3a0584f22b90658551c8b4
kiswayODG <ulrichouedraogo500@gmail.com>
2022-10-04 21:22:21 +0200

    module reservation-createForm

--- GesRessourcesPed/srcApp/forms.py
+++ GesRessourcesPed/srcApp/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from srcApp.models import Reservation
+from crispy_forms.helper import FormHelper
+
 
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
