7ba44557361bcb7bb32f1fa3fd24c8179dd8efa3
Josephilboudo <82779419+Josephilboudo@users.noreply.github.com>
2022-10-10 14:39:33 +0530

    desactivation du mode deboggage

--- GesRessourcesPed/settings.py
+++ GesRessourcesPed/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n(+fz_lrr5a&%x*^@657=cdm^k5++j)2em(==@mqjh1921m!l3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
